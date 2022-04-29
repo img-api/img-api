7fee36593dffe270df46e5a360526d3d95ab36c2
Sergio A. Martinez <sergio@engineer.blue>
2022-04-29 18:48:02 +0000

    [MEDIA] Work on privacy.
    To get a good functional site, we should add privacy, user post
    browsing, tags, and gif support.
    
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- app/user/static/images_download.js
+++ app/user/static/images_download.js
@@ -14,7 +14,10 @@ fetch('/api/media/posts/' + username)
         let media_container = document.getElementById('media_posts_container');
         let html = "";
 
+
         for (media of data.media_files) {
+            let private = (media.is_public ? "" : "checked")
+
             html += `
             <div class="col-lg-4 col-md-12 mb-4">
                 <div class="bg-image hover-overlay ripple shadow-1-strong rounded">
@@ -23,8 +26,22 @@ fetch('/api/media/posts/' + username)
                         <div class="mask" style="background-color: rgba(57, 192, 237, 0.2)"></div>
                 </a>
                 </div>
+            `
+
+            if (media.username == username)
+                html += `
+                <span class='text-white'>
+                    <div class="">
+                        <i class='fa fa-lock'></i>
+                        <label class="form-check-label" for="flexCheckChecked">Private</label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" ${private}/>
+                    </div>
+                </span>
+            `
+
+            html += `
             </div>
-        `;
+            `;
         }
 
         media_container.innerHTML = html
