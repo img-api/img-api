7529e49fd82a11ba3dd7c1e6c8cf9d6270dc7aa3
Sergio A. Martinez <sergio@engineer.blue>
2022-04-30 06:42:18 +0000

    [CI] Mongo didnt start either
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ cd "${BASH_SOURCE%/*}"
 
 sudo apt update
 sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9
-
-python3 --version
 
 echo "VIRTUAL ENV INSTALL"
 
@@ -19,8 +15,8 @@ echo "MONGODB INSTALL"
 sudo apt-get install mongodb-org
 
 echo "START MONGO"
-sudo systemctl start mongodb
-sudo systemctl status mongodb
+sudo systemctl start mongod
+sudo systemctl status mongod
 
 echo "IMAGE MAGICK"
 sudo apt install imagemagick
