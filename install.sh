a2627a3b2fe27bc0072263a125605ace496d6e29
Sergio A. Martinez <sergio@engineer.blue>
2022-04-30 05:43:26 +0000

    [CI] [FIX] urllib request seems to not want to install on the CI environment
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd "${BASH_SOURCE%/*}"
+
+echo "VIRTUAL ENV INSTALL"
+
+sudo apt-get install python3-venv    # If needed
+python3 -m venv .venv
+source .venv/bin/activate
+
+echo "MONGODB INSTALL"
+sudo apt-get install mongodb
