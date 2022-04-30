d96c5bc9a98683f0e878f02ab35f637f96a333b4
Sergio A. Martinez <sergio@engineer.blue>
2022-04-30 06:21:01 +0000

    [CI] Python 3.9
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 cd "${BASH_SOURCE%/*}"
 
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+
+python3 --version
+
 echo "VIRTUAL ENV INSTALL"
 
 sudo apt-get install python3-venv    # If needed
@@ -16,4 +23,3 @@ sudo apt install imagemagick
 
 echo "IMAGE MAGICK"
 sudo apt install redis
-
