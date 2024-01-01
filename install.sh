a66acfe0562800a8c882e25c36dbf50336306023
Sergio A. Martinez <sergio@engineer.blue>
2024-01-01 22:03:57 +0100

    [BUILD] Added libxml2 which seems to fail
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ sudo apt install software-properties-common  -y
 
 echo "VIRTUAL ENV INSTALL"
 
-sudo apt-get install python3-pip python3-venv python3-wheel -y   # If needed
+sudo apt-get install python3-pip python3-venv python3-dev python3-wheel -y   # If needed
+sudo apt-get install libxml2-dev libxslt-dev -y
 
 python3 -m venv .venv
 source .venv/bin/activate
